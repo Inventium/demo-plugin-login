d69cb9a891eb9e8c4ae2a8c17b0cf1d17e6523ec
Dave Doolin <david.doolin@gmail.com>
2010-12-19 14:39:24 -0800

    First commit.

---
+++ demo-plugin-login.php
@@ -0,0 +1,13 @@
+<?php 
+/*
+ * Plugin name: Demo Plugin Login
+ * Plugin URI: http://website-in-a-weekend.net/plugins/demo-plugins/
+ * Description: See who's logged in right now.
+ * Version: 0.1
+ * Author: Dave Doolin
+ * Author URI: http://website-in-a-weekend.net/
+ */
+
+
+
+?>
